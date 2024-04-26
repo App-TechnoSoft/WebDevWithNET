bb46f6dd0a42d398a1658fcf20021bb1d6b1ddff
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-04-26 10:12:58 +0545

    Configure dbcontext

---
+++ CollegeMonitor/Data/Models/Course.cs
@@ -0,0 +1,9 @@
+// Course - Id, Name, TextBook, CreditHrs
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string TextBook { get; set; }
+    public float CreditHrs { get; set; }
+}
